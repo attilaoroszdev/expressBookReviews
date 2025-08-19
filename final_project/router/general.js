ce9614e8dc775288034567893a46fcc894460103
Attila Orosz <attila.orosz.dev@gmail.com>
2025-08-19 18:09:02 +0400

    All tasks completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require('axios');
 
 public_users.post("/register", (req, res) => {
     const username = req.body.username;
@@ -21,57 +21,148 @@ public_users.post("/register", (req, res) => {
     }
 });
 
+// fetchBooks will wrap the `books` object into a promise
+function fetchBooks() {
+    return new Promise((resolve, reject) => {
+        if (books) {
+            resolve(books);
+        } else {
+            reject(new Error('Books not found'));
+        }
+    });
+}
 
-// Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    // return res.status(200).send(JSON.stringify(books, null, 2));
-    // Since we are not using curl, and this is the standard way (plus it looks better in postman
-    return res.status(200).json(books);
+// Here we try to coerce a Promise out of fetchBooks()
+public_users.get('/', async (req, res) => {
+    fetchBooks()
+        .then(books => {
+            res.send(books); // Send the books if found
+        })
+        .catch(error => {
+            res.status(404).json({message: error.message}); //Otherwise 404 error
+        });
 });
 
 
-// Get book details based on ISBN
+// Get the book list available in the shop, original implementation without Promises
+/*
+    public_users.get('/', function (req, res) {
+        // return res.status(200).send(JSON.stringify(books, null, 2));
+        // Since we are not using curl, and this is the standard way (plus it looks better in postman)
+        return res.status(200).json(books);
+    });
+*/
+
+//Wrap the response in an anonymous Promise and return it
+function findBookByISBN(isbn) {
+    return new Promise((resolve, reject) => {
+        if (books[isbn]) {
+            resolve(books[isbn]); // If the book exists, we resolve with the book object
+        } else {
+            reject(new Error('Book with ' + isbn + ' not found')); // Otherwise reject with error msg
+        }
+    });
+}
+
+// Coerce a promise out of books with the right ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    //Write your code here
     const isbn = req.params.isbn;
-    const book = books[isbn];
-    if (book) {
-        book["isbn"] = isbn; //Including the isbn in the response
-        return res.status(200).json(book);
-    } else {
-        return res.status(404).json({message: "ISBN " + isbn + " not found"});
-    }
+    findBookByISBN(isbn)
+        .then(book => {
+            book["isbn"] = isbn; // Including the isbn in the response
+            res.send(book); // Send the book if found
+        })
+        .catch(error => {
+            res.status(404).json({message: error.message}); //Otherwise 404 error
+        });
 });
 
-// Get book details based on author
+// Get book details based on ISBN, original implementation without Promises
+/*
+    public_users.get('/isbn/:isbn', function (req, res) {
+        //Write your code here
+        const isbn = req.params.isbn;
+        const book = books[isbn];
+        if (book) {
+            book["isbn"] = isbn; // Including the isbn in the response
+            return res.status(200).json(book);
+        } else {
+            return res.status(404).json({message: "ISBN " + isbn + " not found"});
+        }
+    });
+*/
+
+// Wrap the whole thing into one big promise
+function findBooksByAuthor(author) {
+    return new Promise((resolve, reject) => {
+        const booksByAuthor = Object.entries(books)
+            // Case-insensitive, includes the ISBN so we can map that to the response
+            .filter(([isbn, book]) => book.author.toLowerCase() === author.toLowerCase())
+            // We might have multiple books by the same author
+            .map(([isbn, book]) => ({...book, isbn}));
+        if (booksByAuthor.length > 0) {
+            resolve(booksByAuthor); // If the book exists, we resolve with the book object
+        } else {
+            reject(new Error("No boobs by '" + author + "'")); // Otherwise reject with error msg
+        }
+    });
+}
+
+// Runs findBooksByAuthor() to get a Promise with the data
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
-    // There might be more than one book by the same author, this seemed the most direct approach
-    const booksByAuthor = Object.entries(books)
-        .filter(([isbn, book]) => book.author.toLowerCase() === author.toLowerCase())  //Case insensiteive, including the isbn so we can map that to the response
-        .map(([isbn, book]) => ({...book, isbn}));
-    if (booksByAuthor.length > 0) {
-        return res.status(200).json(booksByAuthor);
-    } else {
-        return res.status(404).json({message: "Author " + author + " not found"});
-    }
+    findBooksByAuthor(author)
+        .then(booksByAuthor => {
+            res.send(booksByAuthor); // Send the books if found
+        })
+        .catch(error => {
+            res.status(404).json({message: error.message}); //Otherwise 404 error
+        });
 });
 
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title;
-    const booksByTitle = Object.entries(books)
-        // Since we don't have duplicate titles, find will return the object. As the object contains the ISBN,
-        .find(([isbn, book]) => book.title.toLowerCase() === title.toLowerCase())
 
-    if (booksByTitle.length > 0) {
-        //Include the ISBN in the response
-        booksByTitle[1]["isbn"] = booksByTitle[0];
-        // Return the book in the same format as in the search by author
-        return res.status(200).json(booksByTitle[1]);
-    } else {
-        return res.status(404).json({message: "Title " + title + " not found"});
-    }
+// Get book details based on author, original implementation without Promises
+/*
+    public_users.get('/author/:author', function (req, res) {
+        const author = req.params.author;
+        // There might be more than one book by the same author, this seemed the most direct approach
+        const booksByAuthor = Object.entries(books)
+            .filter(([isbn, book]) => book.author.toLowerCase() === author.toLowerCase())  //Case insensiteive, including the isbn so we can map that to the response
+            .map(([isbn, book]) => ({...book, isbn}));
+        if (booksByAuthor.length > 0) {
+            return res.status(200).json(booksByAuthor);
+        } else {
+            return res.status(404).json({message: "Author " + author + " not found"});
+        }
+    });
+*/
+
+// Promises we cannot keep, as usual...
+function findBooksByTitle(title) {
+    return new Promise((resolve, reject) => {
+        const booksByTitle = Object.entries(books)
+            // Since we don't have duplicate titles, find will return the first object, as is.
+            .find(([isbn, book]) => book.title.toLowerCase() === title.toLowerCase())
+        if (booksByTitle && booksByTitle.length > 0) {
+            //Include the ISBN in the response
+            booksByTitle[1]["isbn"] = booksByTitle[0];
+            // Return the book in the same format as in the search by author, for consistency
+            resolve(booksByTitle[1]);
+        } else {
+            reject(new Error("Title '" + title + "' not found"));
+        }
+    });
+}
+
+// Same thing all over again, we try to get that Promised book
+public_users.get('/title/:title', function (req, res) {
+    const bookTitle = req.params.title;
+    findBooksByTitle(bookTitle).then(book => {
+        res.send(book); // Send the book if found
+    }).catch(error => {
+        res.status(404).json({message: error.message}); //Otherwise 404 error
+    });
+
 });
 
 //  Get book review
